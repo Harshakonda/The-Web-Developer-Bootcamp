74259cd777578e7331fc7620a16fac33938258f5
Harshakonda <harsha.konda.hk@gmail.com>
2017-03-10 13:24:52 -0600

    Part-11 Using Var , Strings , methods

---
+++ Part-11/script.js
@@ -0,0 +1,5 @@
+var firstname = prompt("Enter your First Name:");
+var lastname = prompt("Enter your Last Name:");
+var age = prompt("Enter your Age:");
+console.log("User Entered First Name and Last Name is: " + firstname +" "+ lastname);
+console.log("User Entered Age is: " + age + " years Old");
