8823e2705afe74455c462e7ddf9bf4cde5a32ba4
Harshakonda <harsha.konda.hk@gmail.com>
2017-04-15 15:34:27 -0500

    ToDo List Project Done!!!!

--- ToDoListProject/assests/js/todo.js
+++ ToDoListProject/assests/js/todo.js
@@ -1,18 +1,14 @@
-$('li').on('click',function(){
+$('ul').on('click','li',function(){
 	$(this).toggleClass('toogleLis');
 })
-$('li').on('mouseover',function(even){
-	event.stopPropagation();
-	$(this).children('.delete').fadeOut(1000);
-})
-$('.delete').on('click',function(event){
+$('ul').on('click','.delete',function(event){
 	event.stopPropagation();
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	})
 })
 
-$('input').keypress(function(e){
+$('input').on('keypress',function(e){
 	if(e.which === 13){
 		var newTodo = $(this).val();
 		console.log(newTodo)
@@ -21,5 +17,6 @@ $('input').keypress(function(e){
 	}
 })
 $('h3 span').on('click',function(){
-	$('input').toggleClass('plusIcon');
+	//$('input').toggleClass('plusIcon');
+	$('input').fadeToggle(500);
 })
